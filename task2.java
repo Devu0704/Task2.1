6d28889c6a28a7f370d42489256e90a44c7c4b95
Devu0704 <devukrishna.sb1998@gmail.com>
2021-05-11 14:07:02 +0530

    alter in task2.java

--- task2.java
+++ task2.java
@@ -1,8 +1,10 @@
 
 public class task2{
   public static void main(String[] args]{
-    int n=5;
+    int n=50;
     int x=n++;
     int y=n++;
+    int mod=50%5;
+    system.out.println("mod"+mod);
   }
 }
